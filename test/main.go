197444df5d8728f2bc6b474475ef38bb1970ef42
snowmerak <snowmerak@outlook.com>
2022-06-25 22:14:24 +0900

    update test code

--- test/main.go
+++ test/main.go
@@ -4,33 +4,40 @@ import (
 	"context"
 	"fmt"
 	"github.com/snowmerak/timer"
-	"log"
 	"time"
 )
 
 func main() {
-	t := timer.NewTimer(context.Background(), "main", 4)
-
 	two := 0
 	four := 0
 	six := 0
 	eight := 0
 
-	t.Add(time.Second*2, func() {
-		log.Println("two second")
-		two++
-	})
-	t.Add(time.Second*4, func() {
-		log.Println("four seconds")
-		four++
-	})
-	t.Add(time.Second*6, func() {
-		log.Println("six seconds")
-		six++
-	})
-	t.Add(time.Second*8, func() {
-		log.Println("eight seconds")
-		eight++
+	t := timer.NewTimer(context.Background(), "main", []*timer.Schedule{
+		{
+			Action: func() {
+				two++
+			},
+			Interval: time.Second * 2,
+		},
+		{
+			Action: func() {
+				four++
+			},
+			Interval: time.Second * 4,
+		},
+		{
+			Action: func() {
+				six++
+			},
+			Interval: time.Second * 6,
+		},
+		{
+			Action: func() {
+				eight++
+			},
+			Interval: time.Second * 8,
+		},
 	})
 
 	t.Start()
